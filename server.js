2dec161539865f7526aad634295c76530397f6c2
alamarcpsc <alamarcpsc@gmail.com>
2017-04-12 00:37:18 -0700

    path to certs

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ var http  = require('http'),
     fs    = require('fs'),
     app   = require('./app');
 
+/*
 var credentials = {
     key: fs.readFileSync('/etc/letsencrypt/live/adamlamar.me/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/adamlamar.me/cert.pem')
 };
+*/
 
 http.createServer(app).listen(80, function () {
     console.log('http server listening on port: 80');
 });
-
+/*
 https.createServer(credentials, app).listen(443, function () {
     console.log('https server listening on port: 443');
 });
+*/
