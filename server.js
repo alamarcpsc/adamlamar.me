0c052ca619c249449dd6f2ec8218930df746e1e2
alamarcpsc <alamarcpsc@gmail.com>
2017-04-12 00:54:44 -0700

    file reading

--- server.js
+++ server.js
@@ -5,25 +5,15 @@ var http  = require('http'),
     fs    = require('fs'),
     app   = require('./app');
 
-/*
 var credentials = {
     key: fs.readFileSync('/etc/letsencrypt/live/adamlamar.me/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/adamlamar.me/cert.pem')
 };
-*/
 
-var httpServer  = http.createServer(app);
-
-httpServer.listen(80);
-httpServer.on('error', function(err) {
-    console.log('error:' + err);
-});
-httpServer.on('listening', function(){
-    console.log('Listening on port: ' + 80);
+http.createServer(app).listen(80, function () {
+    console.log('http server listening on port: 80');
 });
 
-/*
 https.createServer(credentials, app).listen(443, function () {
     console.log('https server listening on port: 443');
 });
-*/
