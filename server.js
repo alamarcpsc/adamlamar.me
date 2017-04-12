17b3a931d19f10fd246b66aa11e6953c1c2b5543
alamarcpsc <alamarcpsc@gmail.com>
2017-04-12 00:32:18 -0700

    added https

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var http  = require('http'),
     app   = require('./app');
 
 var credentials = {
-    key: fs.readFileSync('/../../../etc/letsencrypt/0001_key-certbot.pem'),
-    cert: fs.readFileSync('/../../../etc/letsencrypt/0001_csr-certbot.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/adamlamar.me/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/adamlamar.me/cert.pem')
 };
 
 http.createServer(app).listen(80, function () {
